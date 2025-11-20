60fbe1b14b8076e231d4725c95c642d1e68b0c23
Earl Luchin <earltroy101@gmail.com>
2025-11-20 08:29:37 +0800

    Added while loop

--- whileloop.js
+++ whileloop.js
@@ -1,6 +1,18 @@
-i = 1;
+/*oujtput = " ";
+x = 1;
 
 while (i <= 5) {
     console.log(i);
     i++;
 }
+
+*/
+arr=[1,2,3];
+output = " ";
+index = 1;
+
+while (index <= arr.length) {
+    output += index + " ";
+    index++;
+}
+console.log(output);
